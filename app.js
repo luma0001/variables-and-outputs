56812a6ff7d165c8ad03d701c7c9ff52cf9ed5d2
SirShagalot <56863638+SirShagalot@users.noreply.github.com>
2023-02-21 14:31:26 +0100

    ...

--- app.js
+++ app.js
@@ -23,4 +23,9 @@ function sub() {
 
 function display() {
   document.querySelector("#number").textContent = number;
+  let messageString = `Taller er nu: ${number}`;
+  document.querySelector("#message").textContent = messageString;
 }
+
+//Giver en error, da vi har en lokal variabel kalt uden for funktionen!
+console.log(messageString);
